80bc3f70e0f8e18ed66d404da842e29ca696057a
Marco Kurzynski <marcokurzynski@icloud.com>
2022-02-07 23:05:44 -0600

    added boost converter class

--- include/mppt.h
+++ include/mppt.h
@@ -2,7 +2,6 @@
 
 #define MPPT_TX PA_12
 #define MPPT_RX PA_11
-
 #define MPPT_BASE_ID 0x0000
 #define MPPT_MODE_ID MPPT_BASE_ID + 8 // mode
 #define MPPT_MOV_ID MPPT_BASE_ID + 10 // max output voltage
@@ -10,21 +9,38 @@
 
 template <typename T>
 struct mutexVar {
-  Mutex mutex;
-  T value;
-  void setValue(unsigned char data[8]) {
-    mutex.lock();
-    memcpy(&value, data, sizeof(T));
-    mutex.unlock();
-  }
+  private:
+    Mutex mutex;
+  public:
+    T value;
+    void setValue(unsigned char data[8]) {
+      mutex.lock();
+      memcpy(&value, data, sizeof(T));
+      mutex.unlock();
+    }
+};
+
+class BoostConverter {
+  private:
+    AnalogIn voltageADC;
+    AnalogIn currentADC;
+  public:
+    BoostConverter(PinName v, PinName i);
+    float voltageIn;
+    float currentIn;
+    void setVoltageIn(void);
+    void setCurrentIn(void);
 };
 
 class Mppt {
   private:
+    BoostConverter BC1;
+    BoostConverter BC2;
+    BoostConverter BC3;
     mutexVar<float> maxOutputVoltage;
     mutexVar<float> maxInputCurrent;
     mutexVar<uint8_t> mode;
-
   public:
+    Mppt();
     bool notParsed(CANMessage msg);
 };
--- src/mppt.cpp
+++ src/mppt.cpp
@@ -1,6 +1,16 @@
 #include "mppt.h"
 #include "mbed.h"
 
+BoostConverter::BoostConverter(PinName v, PinName i) : voltageADC(AnalogIn(v)), currentADC(AnalogIn(i)) {}
+
+void BoostConverter::setCurrentIn(void) {
+  currentIn = currentADC.read_voltage();
+}
+void BoostConverter::setVoltageIn(void) {
+  voltageIn = voltageADC.read_voltage();
+}
+
+Mppt::Mppt() : BC1(BoostConverter(PA_7, PA_6)), BC2(BoostConverter (PA_5, PA_4)), BC3(BoostConverter (PA_3, PA_2)) {}
 
 bool Mppt::notParsed(CANMessage msg) {
   switch (msg.id) {
